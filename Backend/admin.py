05a5977b59eb582111b1a933c365efa79dc06204
Marawan Mogeb Alrahman <85940032+marawan-mogeb@users.noreply.github.com>
2024-12-26 23:52:11 +0200

    Delete Backend directory

--- Backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from backend.models import Review, Watchlist, Favorites
-
-
-admin.site.register(Review)
-admin.site.register(Watchlist)
-admin.site.register(Favorites)
--- Backend/backend/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Root of /homepage/ points to home view
-    path('  /', views.todos, name='Todos'),  # /homepage/todos/ points to todos view
-    
-]
